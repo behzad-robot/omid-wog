e32e54e38b239cd4cb0b857e54448922ab8b47e7
behzad-robot <behzad.robot@gmail.com>
2019-01-26 11:44:34 +0330

    mongo connections

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -17,7 +17,8 @@ export function IS_LOCALHOST()
 }
 export function GetMongoDBURL()
 {
-    return IS_LOCALHOST() ? 'mongodb://localhost:27017/wog' : 'mongodb://determination.ir:27017/wog';
+    // return IS_LOCALHOST() ? 'mongodb://localhost:27017/wog' : 'mongodb://determination.ir:27017/wog';
+    return 'mongodb://localhost:27017/wog';
     // return 'mongodb://localhost:27017/battleship';
     //'mongodb://admin:polo1374@localhost:27017/corridor';
 }
