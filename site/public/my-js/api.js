2f84fc730ee8c489631f8882cddf166938db54d5
behzad-robot <behzad.robot@gmail.com>
2019-01-28 12:36:30 +0330

    hello site

---
+++ site/public/my-js/api.js
@@ -0,0 +1,34 @@
+//api functions
+const API_BASE_URL = "/api/";
+const API = {
+    modelListUrl: (modelName) => (API_BASE_URL + modelName + "/"),
+    modelSingleUrl: (modelName, _id) => (API_BASE_URL + modelName + "/" + _id + "/"),
+};
+const apiCall = (url, next) =>
+{
+    console.log(url);
+    fetch(url,
+        {
+            method : 'GET',
+            mode : "cors",
+            headers: {
+                'api-token': 'ftsb',
+                "admin-token":"hamunhamishegi",
+            }
+        })
+        .then((response) => (response.json()))
+        .then((json) =>
+        {            
+            next(json);
+        });
+}
+const findObjects = (name, next) =>
+{
+    console.log(`findObjects ${name} from ` + API.modelListUrl(name));
+    apiCall(API.modelListUrl(name), next);
+}
+const getObject = (name, _id, next) =>
+{
+    console.log(`getObject ${name} from ` + API.modelListUrl(name));
+    apiCall(API.modelSingleUrl(name,_id), next);
+}
