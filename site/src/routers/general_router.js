6e1dba93fee546b75bec0a412804da1221ecd173
WOG-07 <WOG-07@WOG.local>
2019-02-03 02:04:26 -0800

    items list scroll

--- site/src/routers/general_router.js
+++ site/src/routers/general_router.js
@@ -16,5 +16,8 @@ export default class SiteGeneralRouter extends SiteRouter
         this.router.get('/post',(req,res)=>{
             this.renderTemplate(req,res,'post-single.html',{});
         });
+        this.router.get('/kiri',(req,res)=>{
+            this.renderTemplate(req,res,'slider-kiri.html',{});
+        });
     }
 }
