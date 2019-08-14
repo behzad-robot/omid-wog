830387bb9bfc70d560995d7a9aaabd09963333ec
Behzad AbedinZadeh <Behzad@WOG.local>
2019-08-14 17:54:50 +0430

    few fixes

--- admin/public/my-js/api.js
+++ admin/public/my-js/api.js
@@ -8,7 +8,7 @@ const apiCall = (url, next) =>
 {
     console.log(url);
     console.log(url + '=>' + Date.now());
-    fetch(url + '?_draft=all&limit=800',
+    fetch(url + '?_draft=all&limit=800&isVas=all',
         {
             method: 'GET',
             mode: "cors",
