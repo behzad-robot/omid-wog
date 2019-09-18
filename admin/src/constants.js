76cd837dffdd9ce171241696015a1aba97191b37
Ali Maleki <ali@WOG.local>
2019-09-18 12:38:36 +0430

    admin changed to domain instead of ip

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -5,8 +5,8 @@ export const API_TOKEN = "ftsb";
 export const API_ENCODE_KEY = "omidwog";
 export const API_URL = "http://localhost:8585/api/";
 export const API_BASE_URL = "http://localhost:8585/";
-export const ADMIN_FILE_UPLOAD = IS_LOCALHOST () ? "http://localhost:6565/admin/file-upload" : "http://31.184.135.51:6565/admin/file-upload";
-export const ADMIN_FILE_UPLOAD_NEW = IS_LOCALHOST () ? "http://localhost:6565/admin/files/upload" : "http://31.184.135.51:6565/admin/files/upload";
+export const ADMIN_FILE_UPLOAD = IS_LOCALHOST () ? "http://localhost:6565/admin/file-upload" : "http://worldofgamers.ir:6565/admin/file-upload";
+export const ADMIN_FILE_UPLOAD_NEW = IS_LOCALHOST () ? "http://localhost:6565/admin/files/upload" : "http://worldofgamers.ir:6565/admin/files/upload";
 export const ADMIN_TOKEN = "hamunhamishegi";
 export const ADMIN_URL = "http://localhost:6565/";
 
