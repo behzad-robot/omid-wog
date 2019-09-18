b622489c33a17a4b9bbc4175e0063a66a3ec5fe1
Ali Maleki <ali@WOG.local>
2019-09-18 14:39:20 +0430

    constants new dns

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -5,8 +5,8 @@ export const API_TOKEN = "ftsb";
 export const API_ENCODE_KEY = "omidwog";
 export const API_URL = "http://localhost:8585/api/";
 export const API_BASE_URL = "http://localhost:8585/";
-export const ADMIN_FILE_UPLOAD = IS_LOCALHOST () ? "http://localhost:6565/admin/file-upload" : "http://worldofgamers.ir:6565/admin/file-upload";
-export const ADMIN_FILE_UPLOAD_NEW = IS_LOCALHOST () ? "http://localhost:6565/admin/files/upload" : "http://worldofgamers.ir:6565/admin/files/upload";
+export const ADMIN_FILE_UPLOAD = IS_LOCALHOST() ? "http://localhost:6565/admin/file-upload" : "http://worldofgamers.ir:6565/admin/file-upload";
+export const ADMIN_FILE_UPLOAD_NEW = IS_LOCALHOST() ? "http://localhost:6565/admin/files/upload" : "http://worldofgamers.ir:6565/admin/files/upload";
 export const ADMIN_TOKEN = "hamunhamishegi";
 export const ADMIN_URL = "http://localhost:6565/";
 
@@ -20,7 +20,7 @@ export function IS_LOCALHOST()
 export function GetMongoDBURL()
 {
     // return IS_LOCALHOST() ? 'mongodb://localhost:27017/wog' : 'mongodb://31.184.135.51:27017/wog';
-    return IS_LOCALHOST () ? 'mongodb://admin:admin123@31.184.135.51:27017/wog?authSource=admin' : 'mongodb://admin:admin123@localhost:27017/wog?authSource=admin';
+    return IS_LOCALHOST() ? 'mongodb://admin:admin123@45.82.138.51:27017/wog?authSource=admin' : 'mongodb://admin:admin123@localhost:27017/wog?authSource=admin';
     // return 'mongodb://localhost:27017/battleship';
     //'mongodb://admin:polo1374@localhost:27017/corridor';
 }
--- site-new/src/constants.js
+++ site-new/src/constants.js
@@ -5,7 +5,7 @@ export const API_TOKEN = "ftsb";
 export const API_ENCODE_KEY = "omidwog";
 export const API_URL = "http://localhost:8585/api/";
 export const API_BASE_URL = "http://localhost:8585/";
-export const ADMIN_FILE_UPLOAD = IS_LOCALHOST () ? "http://localhost:6565/admin/file-upload" : "http://31.184.135.51:6565/admin/file-upload";
+export const ADMIN_FILE_UPLOAD = IS_LOCALHOST() ? "http://localhost:6565/admin/file-upload" : "http://31.184.135.51:6565/admin/file-upload";
 export const ADMIN_TOKEN = "hamunhamishegi";
 export const ADMIN_URL = "http://localhost:6565/";
 export const SERVER_FILES_URL = IS_LOCALHOST() ? "http://localhost:8081" : "http://worldofgamers.ir:8080";
@@ -26,7 +26,7 @@ export function IS_LOCALHOST()
 export function GetMongoDBURL()
 {
     // return IS_LOCALHOST() ? 'mongodb://localhost:27017/wog' : 'mongodb://31.184.135.51:27017/wog';
-    return IS_LOCALHOST () ? 'mongodb://admin:admin123@31.184.135.51:27017/wog?authSource=admin' : 'mongodb://admin:admin123@localhost:27017/wog?authSource=admin';
+    return IS_LOCALHOST() ? 'mongodb://admin:admin123@45.82.138.51:27017/wog?authSource=admin' : 'mongodb://admin:admin123@localhost:27017/wog?authSource=admin';
     // return 'mongodb://localhost:27017/battleship';
     //'mongodb://admin:polo1374@localhost:27017/corridor';
 }
